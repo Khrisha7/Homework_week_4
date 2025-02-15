425676c9cea2d822c87cd3116415ff004eca5190
Pooja Murali <p.murali2002@gmail.com>
2025-02-15 09:37:35 +0000

    CREATE: ex12_q1 file EDIT: added code to q1

---
+++ ex12_q1.py
@@ -0,0 +1,11 @@
+cheese = ['Cheddar', 'Stilton', 'Cornish Yarg']
+# cheese += 'Oke'
+# print(cheese)
+# how should 'Oke' be added to the end of the cheese list?
+# you need to add square brackets to the syntax so it prints it as a word
+# cheese += ['Oke']
+# print(cheese)
+
+# how would you add 2 new cheeses to the list with a single command
+cheese += (['Oke', 'Red Leicester'])
+print(cheese)
