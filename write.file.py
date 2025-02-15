bea0bb34602057b03c9949fc3f3004df480d1453
Pooja Murali <p.murali2002@gmail.com>
2025-02-15 11:04:55 +0000

    EDIT: modifed pelican files

--- write.file.py
+++ write.file.py
@@ -6,10 +6,10 @@ bird_file = open('pelican.txt', 'a')
 # i have created a new variable for the first line
 # i have used the .write method to add this line and when you use this method it returns the number of characters added to the file
 first_line = bird_file.write("A wonderful bird is the pelican\n")
-print(first_line)
+
 
 second_line = bird_file.write("His bill holds more than his belican\n")
-print(second_line)
+
 
 # lines is a variable for the list
 # the square brackets show this is list
@@ -17,6 +17,6 @@ print(second_line)
 # the .writelines writes each string from lines variable to the file
 lines = ["He can take in his beak,\n", "Enough food for a week, \n", "But I'm damned if I see how the helican.\n"]
 new_lines = bird_file.writelines(lines)
-print(new_lines)
+
 
 
